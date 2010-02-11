2d8672e03e629a7e500b60b272fbe8e6d9085c72
analogue <none@none>
2010-02-11 15:53:47 -0600

    test commit

--- bin/setenv.cmd
+++ bin/setenv.cmd
@@ -1,4 +1,3 @@
 @rem Add all source and 3rd party libs to python path
-@set PYTHONPATH=resources\src;resources\test;resources\itest;resources\lib\mockito;resources\lib\MySQLdb;resources\lib\MySQLdb\win32;resources\lib\pyxcoder;resources\lib\decorator;resources\lib\odict;resources\lib\BeautifulSoup;resources\lib\elementtree;resources\lib\tvdb_api;resources\lib\themoviedb;resources\lib\IMDbPY;resources\lib\simplejson;resources\lib\nose;resources\lib\shove;resources\lib\durus;resources\lib\mysql-connector-python
-@set PATH=resources\lib\nose\bin;%PATH%
+@set PYTHONPATH=resources\src;resources\test;resources\itest;resources\lib\mockito;resources\lib\MySQLdb;resources\lib\MySQLdb\win32;resources\lib\pyxcoder;resources\lib\decorator;resources\lib\odict;resources\lib\BeautifulSoup;resources\lib\elementtree;resources\lib\tvdb_api;resources\lib\themoviedb;resources\lib\IMDbPY;resources\lib\simplejson;resources\lib\feedparser;resources\lib\shove;resources\lib\durus;resources\lib\mysql-connector-python;resources\lib\python-twitter
 @python -V
