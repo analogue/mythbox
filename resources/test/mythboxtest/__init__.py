eb84219f2f6d077006b32fe57879af3b2db44b7d
Semir Patel <analogue@yahoo.com>
2011-12-21 00:33:38 -0800

    fix broken logger in unit tests

--- resources/test/mythboxtest/__init__.py
+++ resources/test/mythboxtest/__init__.py
@@ -16,19 +16,5 @@ def getLogger(loggerName):
     import logging
     logger  = logging.getLogger(loggerName)
     if not logger.handlers:
-        #print 'loading log config'
-        import logging.config
-        
-        d = getcwd()
-        f = join(d, 'mythbox_log.ini')
-        
-        while not exists(f) and d:
-            d  = getParent(d)
-            f = join(d, 'mythbox_log.ini')
-            
-        if d:
-            #print 'loading log config from %s' % f
-            logging.config.fileConfig(f)
-            logger = logging.getLogger(loggerName)
-            
+        logging.basicConfig()
     return logger
